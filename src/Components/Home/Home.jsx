e356e2eb2cc384e6e152503e190f494589bf329b
Miyka96 <micaelaseminara@outlook.it>
2022-08-11 17:03:24 +0200

    created all files

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import Navbar from '../Partials/Navbar/Navbar'
+
+const Home = () => {
+    return(
+        <>
+            <Navbar/>
+            <p>Home</p>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/Components/Partials/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Navbar = () => {
+    return(
+        <p>navbar</p>
+    )
+}
+
+export default Navbar;
